1f01e5faf6c42bf8c8dfa1f984610f4a6d615eba
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-07-03 18:29:04 -0300

    Add banner and header

--- src/pages/ListaProdutos/ListaProdutos.jsx
+++ src/pages/ListaProdutos/ListaProdutos.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect, useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
 import styles from './ListaProdutos.module.css';
 import { Link } from 'react-router-dom';
+import { Banner } from '../../components/Banner/Banner';
+import { Header } from '../../components/Header/Header';
 
 export const ListaProdutos = () => {
   const [products, setProducts] = useState([]);
@@ -23,31 +25,35 @@ export const ListaProdutos = () => {
   };
 
   return (
-    <div className={styles.listaProdutos}>
-        {products.length === 0 ? (
-          <p>Carregando...</p>
-        ) : (
-            <section className={styles.wrapCards}>
-            {products.map((product) => (
-              <div className={styles.card} key={product.id}>
-                <img src={product.image} alt=""/>
-                <p>{product.name}</p>
-                <p>{product.description}</p>
-                <div className={styles.wrapFlex}>
-                  <p>R${product.price}</p>
-                  <div className={styles.wrapQtd}>
-                    <button onClick={() => removeFromCart(product.id)}>-</button>
-                    <p>{getQuantity(product.id)}</p>
-                    <button onClick={() => addToCart(product)}>+</button>
+    <div>
+      <Header/>
+      <Banner/>
+      <div className={styles.listaProdutos}>
+          {products.length === 0 ? (
+            <p>Carregando...</p>
+          ) : (
+              <section className={styles.wrapCards}>
+              {products.map((product) => (
+                <div className={styles.card} key={product.id}>
+                  <img src={product.image} alt=""/>
+                  <p>{product.name}</p>
+                  <p>{product.description}</p>
+                  <div className={styles.wrapFlex}>
+                    <p>R${product.price}</p>
+                    <div className={styles.wrapQtd}>
+                      <button onClick={() => removeFromCart(product.id)}>-</button>
+                      <p>{getQuantity(product.id)}</p>
+                      <button onClick={() => addToCart(product)}>+</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-            </section>
-        )}
-        <div>
-          <Link to='/carrinho'>Ir para o carrinho</Link>
-        </div>
+              ))}
+              </section>
+          )}
+          <div>
+            <Link to='/carrinho'>Ir para o carrinho</Link>
+          </div>
+      </div>
     </div>
   )
 }
