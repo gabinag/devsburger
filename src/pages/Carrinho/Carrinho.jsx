dff4275cb7b23350b579be9a43bb06d3de6e4eea
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-07-04 22:11:02 -0300

    update style

--- src/pages/Carrinho/Carrinho.jsx
+++ src/pages/Carrinho/Carrinho.jsx
@@ -3,6 +3,8 @@ import { CartContext } from '../../context/CartContext';
 import styles from './Carrinho.module.css';
 import { useNavigate } from 'react-router-dom';
 import { Botao } from '../../components/Botao/Botao';
+import { Header } from '../../components/Header/Header'
+import { Footer } from '../../components/Footer/Footer'
 
 export const Carrinho = () => {
   const navigate = useNavigate();
@@ -13,38 +15,46 @@ export const Carrinho = () => {
   };
 
   return (
-    <div className={styles.carrinho}>
-      {cart.length === 0 ? (
-        <p>Carrinho vazio</p>
-      ) : (
-        <section className={styles.wrapCards}>
-          {cart.map((product) => (
-            <div className={styles.card} key={product.id}>
-              <img src={product.image} alt=""/>
-              <div>
-                  <p>{product.name}</p>
-                  <div className={styles.wrapFlex}>
-                      <p>R${product.price}</p>
-                      <div className={styles.wrapQtd}>
-                        <button onClick={() => removeFromCart(product.id)}>-</button>
-                        <p>{product.quantity}</p>
-                        <button onClick={() => addToCart(product)}>+</button>
+    <>
+      <Header/>
+      <div className={styles.carrinho}>
+        <h1>Carrinho</h1>
+        {cart.length === 0 ? (
+          <p>O carrinho está vazio</p>
+        ) : (
+          <section>
+            <div className={styles.wrapCards}>
+              {cart.map((product) => (
+                <div className={styles.card} key={product.id}>
+                  <img src={product.image} alt=""/>
+                  <div>
+                      <p>{product.name}</p>
+                      <div className={styles.wrapFlex}>
+                          <p>R${product.price}</p>
+                          <div className={styles.wrapQtd}>
+                            <button onClick={() => removeFromCart(product.id)}>-</button>
+                            <p>{product.quantity}</p>
+                            <button onClick={() => addToCart(product)}>+</button>
+                          </div>
                       </div>
                   </div>
-              </div>
+                </div>
+              ))}
+            </div> 
+            <p className={styles.wrapFlexEnd}>Total: R${calculaPrecoTotal().toFixed(2)}</p>
+            <div className={styles.wrapTextArea}>
+              <label htmlFor="obs">Observações:</label>
+              <textarea name="obs" id="obs" placeholder='Alguma observação? Ex.: Sem cebola, sem picles...'></textarea>
             </div>
-          ))}
-        </section>
-      )}
-      <p className={styles.wrapFlexEnd}>Total: R${calculaPrecoTotal().toFixed(2)}</p>
-      <div className={styles.wrapTextArea}>
-        <label htmlFor="obs">Observações:</label>
-        <textarea name="obs" id="obs" placeholder='Alguma observação? Ex.: Sem cebola, sem picles...'></textarea>
-      </div>
-      <div className={styles.wrapFlexEnd}>
-        <Botao onClick={() => navigate('/')} label="Voltar"/>
-        <Botao onClick={() => navigate('/dados')} label="Prosseguir"/>
-      </div>
-    </div>
+            <div className={styles.wrapFlexEnd}>
+              <Botao onClick={() => navigate('/')} label="Voltar"/>
+              <Botao onClick={() => navigate('/dados')} label="Prosseguir"/>
+            </div>
+          </section>
+          )}
+        </div>
+      
+      <Footer/>
+    </>
   );
 };
