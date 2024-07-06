f2440d0b1675571719d7677a4ac763d77f40d9e9
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-07-06 17:16:52 -0300

    update style

--- src/pages/Dados/Dados.jsx
+++ src/pages/Dados/Dados.jsx
@@ -4,6 +4,8 @@ import { CartContext } from '../../context/CartContext';
 import styles from './Dados.module.css';
 import apiDevsBurger from '../../services/apiDevsBurger';
 import { Botao } from '../../components/Botao/Botao';
+import { Header } from '../../components/Header/Header';
+import { Footer } from '../../components/Footer/Footer';
 
 export const Dados = () => {
   const { cart } = useContext(CartContext);
@@ -72,132 +74,136 @@ export const Dados = () => {
   };
 
   return (
-    <div className={styles.dados}>
-        <h1>Preencha os campos a seguir</h1>
-        <form onSubmit={handleSubmit}>
-        <section className={styles.firstSection}>
-          <label htmlFor="name">Nome</label>
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="phone">Telefone</label>
-          <input
-            type="text"
-            name="phone"
-            value={form.phone}
-            onChange={handleChange}
-            required
-          />
-          <div>
-            <p>Selecione a forma de entrega:</p>
-            <div className={styles.wrapRadio}>
-              <div>
-                <input type="radio" id="delivery" name="entrega" value="delivery" checked={deliveryOption === 'delivery'}
-                onChange={handleOptionChange} />
-                <label htmlFor="delivery">Delivery</label>
-              </div>
-              <div>
-                <input type="radio" id="retirar" name="entrega" value="retirar" checked={deliveryOption === 'retirar'}
-                onChange={handleOptionChange} />
-                <label htmlFor="retirar">Retirar na loja</label>
-              </div>
-            </div>
-          </div>
-          {deliveryOption === 'delivery' && (
-          <div>
-            <div>
-              <label htmlFor="cep">CEP</label>
-              <div>
-                <input
-                  type="text"
-                  name="cep"
-                  onChange={handleChange}
-                />
-                <button className={styles.btnCep}>Pesquisar</button>
-              </div>
-            </div>
-            <div className={styles.wrapInputs}>
-              <div>
-                <label htmlFor="logradouro">Logradouro</label>
-                <input
-                  type="text"
-                  name="logradouro"
-                  value={form.address.logradouro}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="numero">Nº</label>
-                <input
-                  type="number"
-                  name="numero"
-                  value={form.address.numero}
-                  onChange={handleChange}
-                  required
-                  className={styles.sizeInput}
-                />
-              </div>
-            </div>
-            <div className={styles.wrapInputs}>
-              <div>
-                <label htmlFor="bairro">Bairro</label>
-                <input
-                  type="text"
-                  name="bairro"
-                  value={form.address.bairro}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="complemento">Complemento</label>
-                <input
-                  type="text"
-                  name="complemento"
-                  value={form.address.complemento}
-                  onChange={handleChange}
-                  className={styles.sizeInput}
-                />
-              </div>
-            </div>
-          </div>
-          )}
-        </section>
-        <section className={styles.secondSection}>
-          <h3>Itens no Carrinho</h3>
-          <div>
-            {cart.map((item) => (
-              <div key={item.id}>
-                <p>{item.quantity}x {item.name} - R${item.price * item.quantity}</p>
-              </div>
-            ))}
-            <p className={styles.total}>Total: R${cart.reduce((total, item) => total + item.price * item.quantity, 0)}</p>
-          </div>
-          <div>
-            <label htmlFor="paymentMethod">Forma de Pagamento: </label>
-            <select
-              name="paymentMethod"
-              value={form.paymentMethod}
+    <div>
+      <Header/>
+      <div className={styles.dados}>
+          <h1>Preencha os campos a seguir</h1>
+          <form onSubmit={handleSubmit}>
+          <section className={styles.firstSection}>
+            <label htmlFor="name">Nome</label>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
               onChange={handleChange}
               required
-            >
-              <option value="" disabled selected hidden>Selecionar</option>
-              <option value="Cartão">Cartão de Crédito</option>
-              <option value="Dinheiro">Dinheiro</option>
-              <option value="Pix">Pix</option>
-            </select>
-          </div>
-          <div className={styles.wrapBtn}>
-            <Botao onClick={() => navigate('/carrinho')} label="Alterar itens"/>
-            <Botao type="submit" label="Finalizar pedido"/>
-          </div>
-        </section>
-      </form>
+            />
+            <label htmlFor="phone">Telefone</label>
+            <input
+              type="text"
+              name="phone"
+              value={form.phone}
+              onChange={handleChange}
+              required
+            />
+            <div>
+              <p>Selecione a forma de entrega:</p>
+              <div className={styles.wrapRadio}>
+                <div>
+                  <input type="radio" id="delivery" name="entrega" value="delivery" checked={deliveryOption === 'delivery'}
+                  onChange={handleOptionChange} />
+                  <label htmlFor="delivery">Delivery</label>
+                </div>
+                <div>
+                  <input type="radio" id="retirar" name="entrega" value="retirar" checked={deliveryOption === 'retirar'}
+                  onChange={handleOptionChange} />
+                  <label htmlFor="retirar">Retirar na loja</label>
+                </div>
+              </div>
+            </div>
+            {deliveryOption === 'delivery' && (
+            <div>
+              <div>
+                <label htmlFor="cep">CEP</label>
+                <div>
+                  <input
+                    type="text"
+                    name="cep"
+                    onChange={handleChange}
+                  />
+                  <button className={styles.btnCep}>Pesquisar</button>
+                </div>
+              </div>
+              <div className={styles.wrapInputs}>
+                <div>
+                  <label htmlFor="logradouro">Logradouro</label>
+                  <input
+                    type="text"
+                    name="logradouro"
+                    value={form.address.logradouro}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="numero">Nº</label>
+                  <input
+                    type="number"
+                    name="numero"
+                    value={form.address.numero}
+                    onChange={handleChange}
+                    required
+                    className={styles.sizeInput}
+                  />
+                </div>
+              </div>
+              <div className={styles.wrapInputs}>
+                <div>
+                  <label htmlFor="bairro">Bairro</label>
+                  <input
+                    type="text"
+                    name="bairro"
+                    value={form.address.bairro}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="complemento">Complemento</label>
+                  <input
+                    type="text"
+                    name="complemento"
+                    value={form.address.complemento}
+                    onChange={handleChange}
+                    className={styles.sizeInput}
+                  />
+                </div>
+              </div>
+            </div>
+            )}
+          </section>
+          <section className={styles.secondSection}>
+            <h3>Itens no Carrinho</h3>
+            <div>
+              {cart.map((item) => (
+                <div key={item.id}>
+                  <p>{item.quantity}x {item.name} - R${item.price * item.quantity}</p>
+                </div>
+              ))}
+              <p className={styles.total}>Total: R${cart.reduce((total, item) => total + item.price * item.quantity, 0)}</p>
+            </div>
+            <div>
+              <label htmlFor="paymentMethod">Forma de Pagamento: </label>
+              <select
+                name="paymentMethod"
+                value={form.paymentMethod}
+                onChange={handleChange}
+                required
+              >
+                <option value="" disabled selected hidden>Selecionar</option>
+                <option value="Cartão">Cartão de Crédito</option>
+                <option value="Dinheiro">Dinheiro</option>
+                <option value="Pix">Pix</option>
+              </select>
+            </div>
+          </section>
+        </form>
+        <div className={styles.wrapBtn}>
+              <Botao onClick={() => navigate('/carrinho')} label="Alterar itens"/>
+              <Botao type="submit" label="Finalizar pedido"/>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
