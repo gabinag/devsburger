5a6ae055b3fdfd11746554d8383d5b666dc21fcc
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-07-10 19:39:51 -0300

    change banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,17 @@
 import styles from './Banner.module.css'
 import { Botao } from '../Botao/Botao'
+import imgBurger from '../../assets/burger.png'
 
 export const Banner = () => {
   return (
     <div className={styles.banner}>
         <div className={styles.content}>
-            <h1>O melhor hambúrguer você encontra no Devs Burger</h1>
-            <p>Veja nossos deliciosos hambúrgueres e faça seu pedido</p>
-            <Botao label="Ver produtos"/>
+            <img src={imgBurger} alt="" />
+            <div>
+              <h1>O melhor hambúrguer você encontra no Devs Burger</h1>
+              <p>Veja nossos deliciosos hambúrgueres e faça seu pedido</p>
+              <Botao label="Ver produtos"/>
+            </div>
         </div>
     </div>
   )
