cea52d61e9d1928a746b423ea0f717755ecda7af
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-08-02 09:12:33 -0300

    add menu hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
-        <nav className={styles.headerNav}>
-            <p>Logo</p>
-            <ul className={styles.headerList}>
-                <li><Link to="/" className={styles.link}>Produtos</Link></li>
-                <li><Link to="/sobre" className={styles.link}>Sobre</Link></li> 
-                <li><Link to="/pedido" className={styles.link}>Meu Pedido</Link></li>
-            </ul>
-        </nav>
+      <nav className={styles.headerNav}>
+        <p>Logo</p>
+        <ul className={`${styles.headerList} ${isMenuOpen ? styles.active : ''}`}>
+            <li><Link to="/" className={styles.link}>Produtos</Link></li>
+            <li><Link to="/sobre" className={styles.link}>Sobre</Link></li> 
+            <li><Link to="/pedido" className={styles.link}>Meu Pedido</Link></li>
+        </ul>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+      </div>
+      </nav>
     </header>
   )
 }
